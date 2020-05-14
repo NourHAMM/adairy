25b5f1f4bdcd05aef85f5b18be18c9dee1bbf2e4
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-05-14 06:15:23 +0400

    N DOne

--- src/views/diary.vue
+++ src/views/diary.vue
@@ -1,3 +1,11 @@
 <template>
-img class="object-contain h-48 object-left-top " src=../assets/img/yhankful/1t.png>
+<div><br />
+<img class="object-contain h-48 object-left-top " src=../assets/diary.jpg>
+<h1 class= "text-gray-700  font-bold"> In our Diary we should have pages to keep track of our good deeds, <br/>
+what we are grateful for , and what we read.</h1>
+<br />
+<h1 class= "text-gray-700  font-bold">Having a journal that is graphed is a great idea.</h1>
+<img class="object-contain h-48 object-left-top " src=../assets/graphed.jpg>
+<h1 class= "text-gray-700  font-bold">The above graphed ready paper helps greatly when journaling!</h1>
+</div>
 </template>
